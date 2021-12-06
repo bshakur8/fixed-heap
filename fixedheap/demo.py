b955d2b6ac7e3100666ee4e6537e357c34454d4b
Bhaa Shakur <bhaa@vastdata.com>
2021-12-06 16:48:32 +0200

    add real min max heaps

--- fixedheap/demo.py
+++ fixedheap/demo.py
@@ -1,4 +1,3 @@
-
 import operator
 import sys
 from itertools import product
@@ -26,7 +25,14 @@ def get_random_list(random_range, data_size):
     return data
 
 
-def get_random_data(size=10, data_size=10, random_range=(1, 100), data_type=Optional[type], keys=None, nested_dict=False):
+def get_random_data(
+    size=10,
+    data_size=10,
+    random_range=(1, 100),
+    data_type=Optional[type],
+    keys=None,
+    nested_dict=False,
+):
     items = {}
     for i in range(size):
         if data_type is dict:
@@ -42,62 +48,89 @@ def get_random_data(size=10, data_size=10, random_range=(1, 100), data_type=Opti
 
 
 def simple_usage(heap_type="max"):
-    items = get_random_data()
+    limit = 3
+    items = get_random_data(size=10, random_range=(0, 20))
+
+    for item in items.values():
+        pprint(f"{item}, {sum(item)}")
 
     # Take a max heap with limit 3
-    heap = HeapFactory.get(heap_type)(limit=3)
+    heap = HeapFactory.get(heap_type)(limit=limit, aggregator=lambda x: sum(x))
 
     # put data in heap; mark keys as items to get after heap
     for key, data in items.items():
         heap.append(key=(key, data), data=data)
 
-    check_heap(heap, heap_type)
+    check_heap(heap, heap_type, limit=limit)
 
 
-def advanced_usage(heap_type='min'):
+def advanced_usage(heap_type="min"):
     # start a max heap with limit 2 and set average aggregator
-    items = get_random_data(size=10, data_type=dict, keys=['east', 'west', 'north', 'south'])
+    limit = 10
+    items = get_random_data(
+        size=10, data_type=dict, keys=["east", "west", "north", "south"]
+    )
 
-    heap = HeapFactory.get(heap_type)(limit=10, aggregator=lambda x: sum(x) / len(x))
+    heap = HeapFactory.get(heap_type)(limit=limit, aggregator=lambda x: sum(x) / len(x))
 
     # put data in heap; mark keys as items to get after heap
     for key, data in items.items():
         heap.append(key=(key, data), data=data)
 
-    check_heap(heap, heap_type)
+    check_heap(heap, heap_type, limit=limit)
 
 
-def check_heap(heap, heap_type):
-    start_value, operate = (sys.maxsize, operator.le) if heap_type == 'max' else (-sys.maxsize, operator.ge)
+def check_heap(heap, heap_type, limit):
+    start_value, operate = (
+        (sys.maxsize, operator.le)
+        if heap_type.startswith("max")
+        else (-sys.maxsize, operator.ge)
+    )
+    assert len(heap) == limit, len(heap)
     # iterate over heap to get sorted items
     for key, data in heap:
         agg = heap.aggregator(data)
         print(f"{key=}, {agg=}")
+        assert isinstance(agg, (float, int)), type(agg)
         assert operate(agg, start_value)
         start_value = agg
 
 
 def advanced_usage_2(heap_type="max"):
+    limit = 2
     # start a max heap with limit 2 and set average aggregator
-    keys = ['east', 'west', 'north', 'south']
+    keys = ["east", "west", "north", "south"]
     items = get_random_data(data_type=dict, keys=keys, nested_dict=True)
 
-    heap = HeapFactory.get(heap_type)(limit=2, aggregator=type_aggregator(parts=['east', 'south']))
+    heap = HeapFactory.get(heap_type)(
+        limit=limit, aggregator=type_aggregator(parts=["east", "south"])
+    )
 
     # put data in heap; mark keys as items to get after heap
     for key, data in items.items():
         heap.append(key=(key, data), data=data)
 
-    check_heap(heap, heap_type)
+    check_heap(heap, heap_type, limit=limit)
 
 
-def main():
-    for case, _type in product((simple_usage, advanced_usage, advanced_usage_2), ['max', 'min']):
+def simple():
+    simple_usage("max_heap")
+
+
+def all_cases():
+    for case, _type in product(
+        (simple_usage, advanced_usage, advanced_usage_2), ["max", "min", "max_heap", 'min_heap']
+    ):
         if _type:
             pprint(f"{case.__name__}, {_type}")
             case(_type)
-            pprint("=-"*80)
+            pprint("=-" * 80)
 
 
-if __name__ == '__main__':
-    main()
+def main():
+    # simple()
+    all_cases()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- fixedheap/heap.py
+++ fixedheap/heap.py
@@ -1,58 +1,15 @@
-import operator
-from abc import ABC
+from abc import ABC, abstractmethod
 from collections import namedtuple
 from enum import Enum
-from heapq import heappush, heapify, heappop, nlargest, heappushpop
-from random import randint, shuffle
-from typing import Any, Dict, Callable
+from heapq import nlargest, nsmallest, heappush
+from random import randint, random
+from typing import Any, Callable, List
 
-__all__ = {'HeapFactory'}
-
-
-def sum_aggregator(obj):
-    return sum(obj)
+__all__ = {"HeapFactory"}
 
 
-class Heap(ABC):
-    """Abstract class of all fixed size heap types"""
-
-    def __init__(self, limit: int, aggregator: Callable = None):
-        self.heap = []
-        self.limit = limit
-        self.aggregator = aggregator or sum_aggregator
-
-    def append(self, key: Any, data: Dict):
-        """Add a new node to heap
-
-        :param key: Node identifier: Data to get after heapify
-        :param data: data to run heap on
-        """
-        self._add_node(HeapNode(key=key, value=self.aggregator(data)))
-
-    def _add_node(self, node):
-        if len(self.heap) < self.limit:
-            heappush(self.heap, node)
-        else:
-            self._handle_limit(node)
-
-    def _handle_limit(self, node):
-        raise NotImplementedError()
-
-    def top_n(self):
-        raise NotImplementedError()
-
-    def __iter__(self):
-        yield from self.top_n()
-
-    def __len__(self):
-        return len(self.heap)
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}: [{len(self.heap)}/{self.limit}]"
-
-
-class HeapNode(namedtuple('Node', 'key value')):
-    """A lightweight heap node: saves data and maintain order"""
+class HeapNode(namedtuple("HeapNode", "key value")):
+    """A lightweight class that saves data and maintain order"""
 
     __slots__ = ()
 
@@ -63,79 +20,155 @@ class HeapNode(namedtuple('Node', 'key value')):
         return self.value < other.value
 
     def __repr__(self):
-        return f"{self.key}: {self.value}"
+        return f"Key={self.key}: Value={self.value}"
 
 
-class MinHeap(Heap):
-    def _handle_limit(self, node):
-        heappush(self.heap, node)
-        max_index = max(enumerate(self.heap), key=operator.itemgetter(1))[0]  # O(n)
-        self.heap.pop(max_index)
+class BaseSorter(ABC):
+    @classmethod
+    @abstractmethod
+    def append(cls, node: HeapNode, heap: "Heap"):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def sort(cls, items, limit):
+        pass
+
+    @classmethod
+    def index(cls, func, items):
+        return func(enumerate(items), key=lambda n: n[1].value)[0]
+
+
+class Sorter(BaseSorter, ABC):
+    @classmethod
+    def append(cls, node: HeapNode, heap: "Heap"):
+        heap.nodes.append(node)
+        if heap.reached_limit():
+            cls.handle_limit(heap.nodes)
+
+    @classmethod
+    @abstractmethod
+    def handle_limit(cls, items: List[HeapNode]):
+        pass
+
+
+class Aggregators:
+    @staticmethod
+    def sum(obj):
+        return sum(obj)
+
+    @staticmethod
+    def dummy(o):
+        return o
+
+
+class Heap:
+    """Class of all fixed size heap types"""
+
+    def __init__(self, sorter: BaseSorter, limit: int, aggregator: Callable = None):
+        self.nodes = []
+        self.sorter = sorter
+        self.limit = limit
+        self.aggregator = aggregator or Aggregators.dummy
+
+    def reached_limit(self):
+        return self.limit < len(self)
+
+    def append(self, key: Any, data: Any):
+        """Add a new node to heap
+
+        :param key: Node identifier: Data to get after heapify
+        :param data: data to run heap on
+        """
+        node = HeapNode(key=key, value=self.aggregator(data))
+        self.sorter.append(node, heap=self)
 
     def top_n(self):
-        heapify(self.heap)
-        while len(self.heap) > 0:
-            yield heappop(self.heap).key
+        for item in self.sorter.sort(self.nodes, self.limit):
+            yield item.key
+
+    def __iter__(self):
+        yield from self.top_n()
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def __repr__(self):
+        return f"Heap-{self.sorter.__class__.__name__}: [{len(self.nodes)}/{self.limit}]"
 
 
-class MaxHeap(Heap):
-    def _handle_limit(self, node: HeapNode):
-        heappushpop(self.heap, node)
+class MinHeap(BaseSorter):
+    @classmethod
+    def append(cls, node: HeapNode, heap: "Heap"):
+        heappush(heap.nodes, node)
+        if heap.reached_limit():
+            heap.nodes.pop(cls.index(max, heap.nodes))  # O(limit)
 
-    def top_n(self):
-        self.heap = nlargest(self.limit, self.heap)
-        while len(self.heap) > 0:
-            yield self.heap.pop(0).key
-
-
-class RandomHeap(Heap):
-    def _add_node(self, node):
-        self.heap.append(node)
-        if len(self.heap) > self.limit:
-            self._handle_limit(node)
-
-    def _handle_limit(self, node: HeapNode):
-        self.heap.pop(randint(0, len(self.heap) - 1))  # nosec
-
-    def top_n(self):
-        shuffle(self.heap)
-        while len(self.heap) > 0:
-            yield self.heap.pop(0).key
+    @classmethod
+    def sort(cls, items, limit):
+        return nsmallest(limit, items)
 
 
-class HeapType(Enum):
-    MIN = "min"
-    MAX = "max"
-    RANDOM = "random"
+class MaxHeap(BaseSorter):
+    @classmethod
+    def append(cls, node: HeapNode, heap: "Heap"):
+        heappush(heap.nodes, node)
+        if heap.reached_limit():
+            heap.nodes.pop(cls.index(min, heap.nodes))  # O(limit)
+
+    @classmethod
+    def sort(cls, items, limit):
+        return nlargest(limit, items)
+
+
+class Min(Sorter):
+    @classmethod
+    def handle_limit(cls, items: List[HeapNode]):
+        items.pop(cls.index(max, items))  # O(limit)
+
+    @classmethod
+    def sort(cls, items, limit):
+        return nsmallest(limit, items)
+
+
+class Max(Sorter):
+    @classmethod
+    def handle_limit(cls, items: List[HeapNode]):
+        items.pop(cls.index(min, items))  # O(limit)
+
+    @classmethod
+    def sort(cls, items, limit):
+        return nlargest(limit, items)
+
+
+class Random(Sorter):
+    @classmethod
+    def handle_limit(cls, items: List):
+        items.pop(randint(0, len(items) - 1))  # nosec
+
+    @classmethod
+    def sort(cls, items, *_, **__):
+        # return a shuffle copy of heap
+        return sorted(items, key=lambda _: random())
+
+
+class SortType(Enum):
+    MIN = Min
+    MAX = Max
+    MIN_HEAP = MinHeap
+    MAX_HEAP = MaxHeap
+    RANDOM = Random
 
 
 class _HeapFactory:
     __slots__ = {}
 
-    _HEAPS_INDEX = {
-        HeapType.MIN: MinHeap,
-        HeapType.MAX: MaxHeap,
-        HeapType.RANDOM: RandomHeap,
-    }
-
     @classmethod
-    def max(cls, limit, aggregator=None):
-        return MaxHeap(limit, aggregator=aggregator)
-
-    @classmethod
-    def min(cls, limit, aggregator=None):
-        return MinHeap(limit, aggregator=aggregator)
-
-    @classmethod
-    def random(cls, limit, aggregator=None):
-        return RandomHeap(limit, aggregator=aggregator)
-
-    @classmethod
-    def get(cls, heap_type: str) -> Callable:
-        heap = cls._HEAPS_INDEX.get(HeapType(heap_type.lower()), MaxHeap)
+    def get(cls, sort_type: str) -> Callable:
+        sorter = SortType[sort_type.upper()].value()
 
         def ctor(limit, aggregator=None) -> Heap:
-            return heap(limit, aggregator=aggregator)
+            return Heap(sorter, limit=limit, aggregator=aggregator)
 
         return ctor
 
